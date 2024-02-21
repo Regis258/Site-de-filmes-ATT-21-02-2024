1faeacdcde11fe8096cafb2898064ed43115f53b
regis <reginaldopalafoz@gmail.com>
2024-02-21 17:34:59 -0300

    confirmação

---
+++ node_aa8652242e1a7.tmp.js
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive site de Filmes e séries</title>
+    <!-- Link do CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- FAV icones -->
+    <link rel="shortcut icon" href="img/img/fav-icon.png" type="image/x-icon">
+    <!-- BOX Icones -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!--
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    -->
+
+
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.html" class="logo">
+                Filmes<span>Vel</span>
+            </a>
+            <!-- Caixa de Pesquisa -->
+            <div class="search-box">
+                <input type="search" name="" id="search-input" placeholder="Pesquisar">
+                <i class='bx bx-search'></i>
+            </div>
+            <!-- Usuário -->
+            <a href="#" class="user">
+                <img src="./img/img/user.jpg" alt="" class="user-img">
+            </a>
+            <!-- Barra de navegação (navbar) -->
+            <div class="navbar">
+                <a href="#Inicio" class="nav-link nav-active">
+                    <i class='bx bx-home'></i>
+                    <span class="nav-link-title">Inicio</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bxs-hot' ></i>
+                    <span class="nav-link-title">Tendência</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-compass' ></i>
+                    <span class="nav-link-title">Explorar</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-tv' ></i>
+                    <span class="nav-link-title">Filmes</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-heart'></i>
+                    <span class="nav-link-title">Favoritos</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-user-circle'></i>
+                    <span class="nav-link-title">Login</span>
+                </a>
+            </div>
+        </div>
+    </header>
+    <!--    Sessão inicio (Home)    -->
+    <section class="home container" id="home">
+        <!-- imagem inicio -->
+        <img src="img/got/Wallpaper-ID-82017-game-of-thrones-season-8-game-of-thrones-1024x576.jpg" alt="" class="home-img">
+        <!-- Texto Inicio -->
+        <div class="home-text">
+            <h1 class="home-title">Game Of Thrones <br>2011 - 2019</h1>
+            <!--
+
+            <h1 class="sinopse"><p>Game of Thrones conta a história de um lugar onde uma força destruiu o equilíbrio
+                das estações, há muito tempo. Em uma terra onde os verões podem durar vários anos e o inverno toda
+                uma vida, as reivindicações e as forças sobrenaturais correm as portas do Reino dos Sete Reinos.
+                A irmandade da Patrulha da Noite busca proteger o reino de cada criatura que pode vir de lá da 
+                Muralha, mas já não tem os recursos necessários para garantir a segurança de todos. Depois de um
+                verão de dez anos, um inverno rigoroso promete chegar com um futuro mais sombrio. Enquanto isso,
+                conspirações e rivalidades correm no jogo político pela disputa do Trono de Ferro, o símbolo 
+                do poder absoluto.</p></h1>
+
+            -->    
+                <a href="" class="watch btn">
+                    <i class='bx bx-right-arrow'></i>
+                    <span>Assista ao Trailer</span>
+                </a>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+    <!-- Link do JavaScript -->
+    <script src="JavaScript/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!--
+            video https://www.youtube.com/watch?v=vwYiYMxUu4o&ab_channel=CarpoolVenom
+            minuto do video 21:50
+
+            icones
+            https://boxicons.com/?query=tv
+
+            fontes 
+            https://fonts.google.com/specimen/Poppins?query=poppins
+
+            swiper
+            https://swiperjs.com/
+            enviar
+
+
+    -->
+</body>
+</html>
---
+++ node_ccf3ab42a704b.tmp.js
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive site de Filmes e séries</title>
+    <!-- Link do CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- FAV icones -->
+    <link rel="shortcut icon" href="img/img/fav-icon.png" type="image/x-icon">
+    <!-- BOX Icones -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.html" class="logo">
+                Filmes<span>Vel</span>
+            </a>
+            <!-- Caixa de Pesquisa -->
+            <div class="search-box">
+                <input type="search" name="" id="search-input" placeholder="Pesquisar">
+                <i class='bx bx-search'></i>
+            </div>
+            <!-- Usuário -->
+            <a href="#" class="user">
+                <img src="./img/img/user.jpg" alt="" class="user-img">
+            </a>
+            <!-- Barra de navegação (navbar) -->
+            <div class="navbar">
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-home'></i>
+                    <span class="nav-link-title">Inicio</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bxs-hot' ></i>
+                    <span class="nav-link-title">Tendência</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-compass' ></i>
+                    <span class="nav-link-title">Explorar</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-tv' ></i>
+                    <span class="nav-link-title">Filmes</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-home'></i>
+                    <span class="nav-link-title">Inicio</span>
+                </a>
+                <a href="#Inicio" class="nav-link">
+                    <i class='bx bx-home'></i>
+                    <span class="nav-link-title">Inicio</span>
+                </a>
+            </div>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+
+
+    <!-- Link do JavaScript -->
+    <script src="JavaScript/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!--
+            video https://www.youtube.com/watch?v=vwYiYMxUu4o&ab_channel=CarpoolVenom
+    -->
+</body>
+</html>
